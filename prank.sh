10c1173419814ddc2a2b3fa9815cbee01c9c60dd
Ian Macalinao <ianmacalinao@gmail.com>
2012-05-27 18:30:46 -0500

    Forgot to echo. As you can see, I am not very good at this.

--- prank.sh
+++ prank.sh
@@ -1,6 +1,6 @@
 APOCALYPSE="2012/05/27 06:30:00"
 
-sleep $($(date -j -f "%Y-%m-%d %H:%M:%S" "$($APOCALYPSE)" +%s) - $(date +%s))
+sleep $($(date -j -f "%Y-%m-%d %H:%M:%S" "$(echo $APOCALYPSE)" +%s) - $(date +%s))
 
 while true; do
    osascript -e "set Volume 10"
